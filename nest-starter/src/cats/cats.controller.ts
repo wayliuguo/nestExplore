b53bb9837173f306fb1aeca55ebf2e07d8c919c7
liuguowei <1085550655@qq.com>
2025-11-30 23:04:24 +0800

    feat: nest-starter

---
+++ nest-starter/src/cats/cats.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  Header,
+  Query,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  // @HttpCode(204)
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  @Header('Cache-Control', 'no-store')
+  findAll(@Query('breed') breed?: string) {
+    return this.catsService.findAll(breed);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.catsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return this.catsService.update(+id, updateCatDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.catsService.remove(+id);
+  }
+}
---
+++ nest-starter/src/cats/cats.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
+
+@Injectable()
+export class CatsService {
+  create(createCatDto: CreateCatDto) {
+    return `create cat: ${createCatDto.name}, ${createCatDto.age}, ${createCatDto.breed}`;
+  }
+
+  findAll(breed?: string) {
+    return `This action returns all cats of breed ${breed || 'all'}`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} cat`;
+  }
+
+  update(id: number, updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} cat`;
+  }
+}
