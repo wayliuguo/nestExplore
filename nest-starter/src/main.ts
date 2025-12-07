4b3268cf79d4bfa27ff4b855d5f054ab84b1576f
liuguowei <1085550655@qq.com>
2025-12-07 18:02:13 +0800

    feat: 全局中间件

--- nest-starter/src/main.ts
+++ nest-starter/src/main.ts
@@ -7,14 +7,15 @@ import { SimpleLoggerMiddleware } from './common/middleware/logger/simple-logger
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // 启用 DTO 验证
-  
+
   // 全局应用类中间件（有依赖注入的情况）
   const loggerMiddleware = app.get(LoggerMiddleware);
-  app.use(loggerMiddleware.use.bind(loggerMiddleware));
-  
+  // app.use(loggerMiddleware.use.bind(loggerMiddleware));
+  app.use((req, res, next) => loggerMiddleware.use(req, res, next));
+
   // 全局应用函数中间件（可选，根据需要保留或移除）
   // app.use(SimpleLoggerMiddleware);
-  
+
   await app.listen(3000);
 }
 bootstrap();
