522ee32955de743ac535991a7491590d83c9bd76
liuguowei <1085550655@qq.com>
2026-01-05 22:10:41 +0800

    feat: nest 操作 redis

---
+++ nest-redis/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisClientType } from 'redis';
+
+@Injectable()
+export class AppService {
+  @Inject('REDIS_CLIENT')
+  private redisClient: RedisClientType;
+
+  async getHello() {
+    const value = await this.redisClient.keys('*');
+    return value;
+  }
+}
