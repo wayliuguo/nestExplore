2fc1bf00c077926a35cf9ee50fd975d1cb7ccbe4
guoweiwliu <guoweiwliu@lexin.com>
2026-01-10 14:49:13 +0800

    feat: typeorm+mysql+jwt 登录注册

--- login-and-register/src/app.module.ts
+++ login-and-register/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'login_test',
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+    }),
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: 'secret-str',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ login-and-register/src/user/user.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
+import { LoginGuard } from 'src/login.guard';
+
+@Controller('user')
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  @Post('login')
+  async login(
+    @Body() user: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const foundUser = await this.userService.login(user);
+
+    if (foundUser) {
+      const token = await this.jwtService.signAsync({
+        user: {
+          id: foundUser.id,
+          username: foundUser.username,
+        },
+      });
+      res.setHeader('token', token);
+      return 'login success';
+    } else {
+      return 'login fail';
+    }
+  }
+
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    return await this.userService.register(registerDto);
+  }
+
+  @Get('aaa')
+  @UseGuards(LoginGuard)
+  aaa() {
+    return 'aaa';
+  }
+
+  @Get('bbb')
+  @UseGuards(LoginGuard)
+  bbb() {
+    return 'bbb';
+  }
+}
