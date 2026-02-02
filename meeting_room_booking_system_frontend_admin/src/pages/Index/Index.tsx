d9d3d3d615cf20d2590869824835699ba4fae8bc
liuguowei <1085550655@qq.com>
2026-02-02 22:29:24 +0800

    feat: 会议室预定系统-前端界面

---
+++ meeting_room_booking_system_frontend_admin/src/pages/Index/Index.tsx
@@ -0,0 +1,28 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Link, Outlet } from "react-router-dom";
+import './index.css';
+import { useEffect } from "react";
+
+export function Index() {
+
+    useEffect(()=> {
+        const userInfo = localStorage.getItem('user_info');
+        if(!userInfo) {
+            window.location.href = "/login";
+        }
+    }, []);
+
+    return <div id="index-container">
+        <div className="header">
+            <Link to="/" className="sys_name">
+                <h1>会议室预定系统-后台管理</h1>
+            </Link>
+            <Link to="/user/info_modify">
+                <UserOutlined className="icon"/>
+            </Link>
+        </div>
+        <div className="body">
+            <Outlet></Outlet>
+        </div>
+    </div>
+}
